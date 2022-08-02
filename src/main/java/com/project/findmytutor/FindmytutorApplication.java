9e1952e1806d4f334b8130f09f296cbee78c4bf5
Gyu Park <kyesung8282@gmail.com>
2022-08-02 03:20:18 -0400

    First commit

---
+++ src/main/java/com/project/findmytutor/FindmytutorApplication.java
@@ -0,0 +1,13 @@
+package com.project.findmytutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmytutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmytutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/findmytutor/FindmytutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.findmytutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindmytutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
