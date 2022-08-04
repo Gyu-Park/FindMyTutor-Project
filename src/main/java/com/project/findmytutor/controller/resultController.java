3414ec18dd8ddb2c7e9e6dfd024f3f340f6f2edf
Gyu Park <kyesung8282@gmail.com>
2022-08-04 07:49:12 -0400

    Update controllers

--- src/main/java/com/project/findmytutor/controller/resultController.java
+++ src/main/java/com/project/findmytutor/controller/resultController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class resultController {
+public class ResultController {
     
     @GetMapping("/result")
     public String result() {
