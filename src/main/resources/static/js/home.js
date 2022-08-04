c9eaf95250015ee1e01b54698c54c9ea1860563a
Gyu Park <kyesung8282@gmail.com>
2022-08-04 06:39:37 -0400

    Update the result page

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -8,10 +8,22 @@ let cur = 1;
 
 for (let i = 0; i < nextBtn.length; i++) {
     nextBtn[i].addEventListener("click", function() {
-        if (i < 2)
+        if (i == 0) {
             slidePage.style.marginLeft = "-25%";
-        else
+            document.getElementById("method").value = "inperson";
+        } else if (i == 1) {
+            slidePage.style.marginLeft = "-25%";
+            document.getElementById("method").value = "online";
+        } else if (i == 2) {
             slidePage.style.marginLeft = "-50%";
+            document.getElementById("subject").value = "programming";
+        } else if (i == 3) {
+            slidePage.style.marginLeft = "-50%";
+            document.getElementById("subject").value = "math";
+        } else {
+            slidePage.style.marginLeft = "-50%";
+            document.getElementById("subject").value = "english";
+        }
         bullet[cur - 1].classList.add("active");
         progressCheck[cur - 1].classList.add("active");
         cur += 1;
@@ -19,6 +31,13 @@ for (let i = 0; i < nextBtn.length; i++) {
 }
 for (let i = 0; i < submitBtn.length; i++) {
     submitBtn[i].addEventListener("click", function() {
+        if (i == 0) {
+            document.getElementById("price").value = "40-50";
+        } else if (i == 1) {
+            document.getElementById("price").value = "50-60";
+        } else {
+            document.getElementById("price").value = "60-70";
+        }
         bullet[cur - 1].classList.add("active");
         progressCheck[cur - 1].classList.add("active");
         cur += 1;
