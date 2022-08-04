4174c0ed226f93f0088c846f928971e2e11d2767
Gyu Park <kyesung8282@gmail.com>
2022-08-04 07:57:11 -0400

    Update Tutor DTO

--- src/main/java/com/project/findmytutor/model/Tutor.java
+++ src/main/java/com/project/findmytutor/model/Tutor.java
@@ -13,17 +13,21 @@ import lombok.Setter;
 public class Tutor {
     String name;
     String phone;
+    String location;
     List<String> subject;
     String priceRange;
     String picture;
 
-    public Tutor(String name, String phone, List<String> subject, String priceRange, String picture) {
+    public Tutor() {
+
+    }
+
+    public Tutor(String name, String phone, String location, List<String> subject, String priceRange, String picture) {
         this.name = name;
         this.phone = phone;
+        this.location = location;
         this.subject = subject;
         this.priceRange = priceRange;
         this.picture = picture;
     }
-
-    
 }
