61d68de652e7228e92b203c3a5d7fd3b0af1f90a
Gyu Park <kyesung8282@gmail.com>
2022-08-04 08:19:02 -0400

    Update Tutor and Student DTO

--- src/main/java/com/project/findmytutor/model/Student.java
+++ src/main/java/com/project/findmytutor/model/Student.java
@@ -1,7 +1,5 @@
 package com.project.findmytutor.model;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,51 +12,37 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Tutor {
-    
+public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long tutorId;
+    private Long studentId;
 
     @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
-    private String password;
+	private String password;
 
     @Column(nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column
     private String phone;
 
-    @Column(nullable = false)
+    @Column
     private String location;
 
-    @Column(nullable = false)
-    private List<String> subject;
-
-    @Column(nullable = false)
-    private String priceRange;
-
-    @Column
-    private String picture;
-
-    public Tutor() {
+    public Student() {
 
     }
 
-    public Tutor(Long tutorId, String email, String password, String name, String phone, String location,
-            List<String> subject, String priceRange, String picture) {
-        this.tutorId = tutorId;
+    public Student(Long studentId, String email, String password, String name, String phone, String location) {
+        this.studentId = studentId;
         this.email = email;
         this.password = password;
         this.name = name;
         this.phone = phone;
         this.location = location;
-        this.subject = subject;
-        this.priceRange = priceRange;
-        this.picture = picture;
     }
-    
+
 }
