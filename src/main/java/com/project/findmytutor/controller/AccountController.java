d4b409b2fcc52671446d4afda531f2c32673415f
Gyu Park <kyesung8282@gmail.com>
2022-08-24 02:28:15 -0400

    Add tutorRequest page

--- src/main/java/com/project/findmytutor/controller/AccountController.java
+++ src/main/java/com/project/findmytutor/controller/AccountController.java
@@ -2,13 +2,20 @@ package com.project.findmytutor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/account")
 public class AccountController {
 
-    @GetMapping("/account")
+    @GetMapping("")
     public String account() {
         return "account";
+    }
+
+    @GetMapping("/tutorRequest")
+    public String tutorRequest() {
+        return "tutorRequest";
     }
     
 }
