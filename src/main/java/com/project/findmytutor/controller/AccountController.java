20e8b8bc6b6f4eba2f7c8fee2eafb97781e85f4e
Gyu Park <kyesung8282@gmail.com>
2022-08-24 03:09:09 -0400

    Update tutoring btn to show only tutors

--- src/main/java/com/project/findmytutor/controller/AccountController.java
+++ src/main/java/com/project/findmytutor/controller/AccountController.java
@@ -1,15 +1,33 @@
 package com.project.findmytutor.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.project.findmytutor.dto.response.MemberResponse;
+import com.project.findmytutor.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequestMapping("/account")
+@RequiredArgsConstructor
 public class AccountController {
 
+    private final MemberService memberService;
+
     @GetMapping("")
-    public String account() {
+    public String account(HttpServletRequest request, Model model) {
+        try {
+            MemberResponse memberResponse = memberService.getMemberInfo(request.getUserPrincipal().getName());
+            boolean isTutor = memberResponse.getRole().equals("tutor") ? true : false;
+            model.addAttribute("isTutor", isTutor);
+        } catch(NullPointerException e) {
+            return "redirect:/";
+        }
         return "account";
     }
 
