519ce1cd8f3b807ebfb851699c8f6a6682f3c215
Gyu Park <kyesung8282@gmail.com>
2022-08-24 09:43:18 -0400

    Remove lesson post function

--- src/main/resources/static/js/tutorRequest.js
+++ src/main/resources/static/js/tutorRequest.js
@@ -1,34 +1,3 @@
-// sign-up
-$("#btn-post").on('click', function(e) {
-    e.preventDefault();
-
-    var method = document.querySelector('.method:checked').value;
-    var subject = document.querySelector('.subject:checked').value;
-    
-    var lessonPostRequest = {
-                    method: method,
-                    subject: subject,
-                    price: $("#post-price").val(),
-                    member: null
-                };
-
-    $.ajax({
-           type: "POST",
-           url: "/api/lesson/post",
-           data: JSON.stringify(lessonPostRequest),
-           contentType : "application/json; charset=utf-8",
-           success: function(data)
-           {
-                alert("Successfully posted!");
-                closePostForm();
-           },
-           error: function(data) {
-               alert(data);
-               console.log("error");
-           }
-         });
-});
-
 // post form
 function openPostForm() {
     document.body.classList.add("showPostForm");
