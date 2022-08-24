58941c7bb4f2efe16d0d4ee190f3cf82e18de6c3
Gyu Park <kyesung8282@gmail.com>
2022-08-24 09:56:13 -0400

    Add lessons for testing

--- src/main/java/com/project/findmytutor/FindmytutorApplication.java
+++ src/main/java/com/project/findmytutor/FindmytutorApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.project.findmytutor.domain.Lesson;
 import com.project.findmytutor.domain.Member;
+import com.project.findmytutor.repository.LessonRepository;
 import com.project.findmytutor.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -22,10 +24,34 @@ public class FindmytutorApplication {
 	}
 
 	@Bean
-	ApplicationRunner applicationRunner(MemberRepository memberRepository) {
+	ApplicationRunner applicationRunner(MemberRepository memberRepository, LessonRepository lessonRepository) {
 		return args -> {
-			memberRepository.save(new Member(null, "gp2693@gmail.com", passwordEncoder.encode("1234"), "Gyuseok Park", "0123456789", "New York", "tutor"));
-			memberRepository.save(new Member(null, "gp2693@naver.com", passwordEncoder.encode("1234"), "Gs Park", "9876543210", "New Jersey", "student"));
+			memberRepository.save(new Member(null, 
+											"gp2693@gmail.com", 
+											passwordEncoder.encode("1234"), 
+											"Gyuseok Park", 
+											"0123456789", 
+											"New York", 
+											"tutor"));
+			memberRepository.save(new Member(null, 
+											"gp2693@naver.com", 
+											passwordEncoder.encode("1234"), 
+											"Gs Park", 
+											"9876543210", 
+											"New Jersey", 
+											"student"));
+			lessonRepository.save(new Lesson(null, 
+											"online", 
+											"programming", 
+											"40", 
+											null, 
+											memberRepository.findByEmail("gp2693@gmail.com").orElseThrow()));
+			lessonRepository.save(new Lesson(null, 
+											"online", 
+											"programming", 
+											"45", 
+											null, 
+											memberRepository.findByEmail("gp2693@gmail.com").orElseThrow()));
 		};
 	}
 }
