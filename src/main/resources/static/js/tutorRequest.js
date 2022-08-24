9aca2dac4c9e5a52fc583808c46b300989e9cd73
Gyu Park <kyesung8282@gmail.com>
2022-08-24 04:51:45 -0400

    Update tutorRequest page

---
+++ src/main/resources/static/js/tutorRequest.js
@@ -0,0 +1,41 @@
+// sign-up
+$("#btn-signup").on('click', function(e) {
+    e.preventDefault();
+    var role;
+    if (document.getElementById("role-tutor").checked)
+        role = document.getElementById("role-tutor").value;
+    else
+        role = document.getElementById("role-student").value;
+    var signupRequest = {
+                    role: role,
+                    firstName: $("#signup-firstName").val(),
+                    lastName: $("#signup-lastName").val(),
+                    email: $("#signup-email").val(),
+                    password: $("#signup-password").val()
+                };
+
+    $.ajax({
+           type: "POST",
+           url: "/api/auth/signup",
+           data: JSON.stringify(signupRequest),
+           contentType : "application/json; charset=utf-8",
+           success: function(data)
+           {
+                alert("Now you're a member!");
+                closeRegistrationForm();
+                openLoginForm();
+           },
+           error: function(data) {
+               alert(data);
+               console.log("error");
+           }
+         });
+});
+
+// post form
+function openPostForm() {
+    document.body.classList.add("showPostForm");
+}
+function closePostForm() {
+    document.body.classList.remove("showPostForm");
+}
