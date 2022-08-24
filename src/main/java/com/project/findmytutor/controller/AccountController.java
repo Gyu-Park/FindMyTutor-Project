a37c058e94d9a5deccb9763aef41f97cdfdceb56
Gyu Park <kyesung8282@gmail.com>
2022-08-23 22:53:14 -0400

    Commit account page and controller

---
+++ src/main/java/com/project/findmytutor/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.project.findmytutor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/account")
+    public String account() {
+        return "account";
+    }
+    
+}
