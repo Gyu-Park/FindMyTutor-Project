b70f4b3278757e30c47b9baa0b08229bac4b28d6
Gyu Park <kyesung8282@gmail.com>
2022-08-26 11:20:08 -0400

    Update request cards get ajax

--- src/main/resources/static/js/tutoringRequest.js
+++ src/main/resources/static/js/tutoringRequest.js
@@ -25,3 +25,35 @@ $("#btn-request").on('click', function(e) {
            }
          });
 });
+
+if(document.URL.indexOf("tutoringRequest") >= 0){
+    $.get("/api/requestCard/getAllrequestCards", function(data, status){
+        for (let i = 0; i < data.length; i++) {
+            const messageWrapper = document.querySelector(".message-wrapper");
+            messageWrapper.innerHTML = "<div class=\"message-box\">"
+                                +           "<div class=\"lesson-info\">"
+                                +               "<div class=\"lesson-info-hashtag\" type=\"button\">#" + data[i].lesson.method + "</div>"
+                                +               "<div class=\"lesson-info-hashtag\" type=\"button\">#" + data[i].lesson.subject + "</div>"
+                                +               "<div class=\"lesson-info-hashtag\" type=\"button\">#" + data[i].lesson.price + "</div>"
+                                +           "</div>"
+                                +           "<div class=\"sender-info\">"
+                                +               "<div class=\"col-30\">"
+                                +                   "<span>Name: </span>"
+                                +                   "<span>Email: </span>"
+                                +                   "<span>Phone: </span>"
+                                +                   "<span>Message: </span>"
+                                +               "</div>"
+                                +               "<div class=\"col-4\"></div>"
+                                +               "<div class=\"col-66\">"
+                                +                   "<span>" + data[i].senderName + "</span>"
+                                +                   "<span>" + data[i].senderEmail + "</span>"
+                                +                   "<span>" + data[i].senderPhone + "</span>"
+                                +                   "<span>" + data[i].senderMessage + "</span>"
+                                +               "</div>"
+                                +           "</div>"
+                                +           "<button class=\"check-btn\">Mark as read</button>"
+                                +           "<button class=\"delete-btn\">Delete</button>"
+                                +       "</div>";
+        }
+    });
+}
