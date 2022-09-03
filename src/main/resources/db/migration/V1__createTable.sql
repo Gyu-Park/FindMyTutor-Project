1db117dcafaffd8c661d1a561bce25e6a8502a8c
Gyu-Park <kyesung8282@gmail.com>
2022-09-03 10:50:18 -0400

    Change postgresql

--- src/main/resources/db/migration/V1__createTable.sql
+++ src/main/resources/db/migration/V1__createTable.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS member (
-    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL NOT NULL PRIMARY KEY,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(100) NOT NULL,
     name VARCHAR(50) NOT NULL,
@@ -9,21 +9,20 @@ CREATE TABLE IF NOT EXISTS member (
 );
 
 CREATE TABLE IF NOT EXISTS lesson (
-    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL NOT NULL PRIMARY KEY,
     method VARCHAR(50) NOT NULL,
     subject VARCHAR(100) NOT NULL,
     price VARCHAR(50) NOT NULL,
     rating VARCHAR(50) NULL,
-    member BIGINT NOT NULL 
+    member_id BIGINT NOT NULL 
         CONSTRAINT lesson_member_fkey REFERENCES member
 );
 
 CREATE TABLE IF NOT EXISTS request_card (
-    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL NOT NULL PRIMARY KEY,
     sender VARCHAR(50) NOT NULL,
     receiver VARCHAR(100) NOT NULL,
     message VARCHAR(50) NULL,
-    PRIMARY KEY (id),
-    lesson BIGINT NOT NULL
+    lesson_id BIGINT NOT NULL
         CONSTRAINT request_card_lesson_fkey REFERENCES lesson
 );
